7e62e237632ec8f8d7ab829e3b9f8d993ba8e40e
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2025-04-22 08:32:22 -0300

    adição de variaveis

---
+++ Projeto JS - Lista de Tarefas/index.js
@@ -0,0 +1,5 @@
+const form = document.querySelector('#todo-form')
+const taskTitleInput = document.querySelector('#task-title-input')
+const addTaskButton = document.querySelector('#add-task-button')
+const todoListUl = document.querySelector('#todo-list')
+
