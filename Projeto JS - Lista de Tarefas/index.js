bad42354fcded339b4bed94c6298270d88b8a579
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2025-04-23 11:13:45 -0300

    array tasks atualiza estado de tarefa

--- Projeto JS - Lista de Tarefas/index.js
+++ Projeto JS - Lista de Tarefas/index.js
@@ -3,3 +3,58 @@ const taskTitleInput = document.querySelector('#task-title-input')
 const addTaskButton = document.querySelector('#add-task-button')
 const todoListUl = document.querySelector('#todo-list')
 
+let tasks = []
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const taskTitle = taskTitleInput.value 
+
+    if(taskTitle.length < 5){
+        alert('Sua tarefa deve ter pelo menos 5 caracteres!')
+        return
+    }
+
+    tasks.push({
+        title: taskTitle,
+        done: false
+    })
+
+    const li = document.createElement('li')
+    
+    const input = document.createElement('input')
+    input.setAttribute('type', 'checkbox')
+    input.addEventListener('change', (event) => {
+        const liToToggle = event.target.parentElement
+
+        const spanToToggle = liToToggle.querySelector('span')
+        const done = event.target.checked
+        if(done){
+            spanToToggle.style.textDecoration = 'line-through'
+        }else{
+            spanToToggle.style.textDecoration = 'none'
+        }
+
+        tasks = tasks.map(t => {
+            if(t.title === spanToToggle.textContent){
+                return {
+                    title: t.title,
+                    done: !t.done
+                }
+            }
+            return t
+        })
+
+    })
+
+    const span = document.createElement('span')
+    span.textContent = taskTitle
+
+    li.appendChild(input)
+    li.appendChild(span)
+    li.appendChild(button)
+
+    todoListUl.appendChild(li)
+
+    taskTitleInput.value = ''
+})
